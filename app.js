ff144ca6fa6040838d62bdd94415c2fc0bfd85da
Pritish Jadon <pritishjr07@gmail.com>
2025-01-30 20:31:00 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI('AIzaSyCWvBXfze93N7Gz1mBN2KZ1VPlmQEiU7LI');
+
+app.post('/getResponse', async (req, res)=>{
+    console.log(req.body.question);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+    const result = await model.generateContent(req.body.question);
+    console.log(result.response.text());
+    res.status(200).json({
+        response : result.response.text()
+    })
+}) 
+
+module.exports = app;
