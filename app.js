d9f6713a16e8a4319feb0b6e771163f80e3f135d
Pritish Jadon <pritishjr07@gmail.com>
2025-01-30 21:55:06 +0530

    added vercel.json

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ const genAI = new GoogleGenerativeAI('AIzaSyCWvBXfze93N7Gz1mBN2KZ1VPlmQEiU7LI');
 app.post('/getResponse', async (req, res)=>{
     console.log(req.body.question);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    
+
     const result = await model.generateContent(req.body.question);
     console.log(result.response.text());
     res.status(200).json({
         response : result.response.text()
     })
 }) 
+
+app.get('*', (req,res)=>{
+    res.status(404).json({
+        msg : 'bad request'
+    })
+})
 
 module.exports = app;
